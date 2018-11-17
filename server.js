e4f7e93c519c4d5b80847594370b25c75d6a6b86
Ivana Livaja <ivana.livaja@hotmail.com>
2018-11-17 19:48:51 +0100

    added index.html, style.css, .env

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var mongo = require('mongodb');
-var mogoose = require('mongoose');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var cors = require('cors');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res) {
+	res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// first API endpoint...
+app.get('/api/hello', function(req, res) {
+	res.json({ greeting: 'hello API' });
+});
 
 app.listen(port, function() {
 	console.log('Node.js is listening on port ' + port);
