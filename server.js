6d22a5c2280e4c97ed99a1743c9bbb3c33d2fa76
Ivana Livaja <ivana.livaja@hotmail.com>
2019-07-10 15:23:57 +0200

    final commit

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost/url-short';
-const validUrl = require('valid-url');
-const shortid = require('shortid');
-const errorUrl = 'http://localhost/error';
-const cors = require('cors');
-
-require('./models/UrlShorten');
+const connectDB = require('./config/db');
 
 const app = express();
 
-const port = 3000;
+//Connect to database
+connectDB();
 
-mongoose.connect(mongoURL, { useNewUrlParser: true });
-
-app.use(cors());
-//logger
-app.use('/', (req, res, next) => {
-	console.log(`${req.method}, ${req.path} - ${req.ip}`);
-	next();
-});
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -30,49 +14,27 @@ app.get('/', function(req, res) {
 	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('api/item/:url(*)', async (req, res) => {
-	const urlCode = req.params.url;
-	//return res.json({ urlCode });
-	console.log(urlCode);
-	const item = await UrlShorten.findOne({ urlCode: urlCode });
-	console.log(urlCode);
-	if (item) {
-		return res.redirect(item.originalUrl);
-	} else {
-		return res.redirect(errorUrl);
-	}
+//Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+//logger
+app.use('/', (req, res, next) => {
+	console.log(`${req.method}, ${req.path} - ${req.ip}`);
+	next();
 });
 
-app.post('/api/shorturl/new', async (req, res) => {
-	const { originalUrl, shortBaseUrl } = req.body;
-	if (validUrl.isUri(shortBaseUrl)) {
-	} else {
-		return res.status(401).json('Invalid Base Url');
-	}
-
-	const urlCode = shortid.generate();
-	if (validUrl.isUri(originalUrl)) {
-		try {
-			let item = await UrlShorten.findOne({ originalUrl: originalUrl });
-			if (item) {
-				res.status(200).json(item);
-			} else {
-				shortUrl = shortBaseUrl + '/' + urlCode;
-				let item = new UrlShorten({
-					originalUrl,
-					shortUrl,
-					urlCode
-				});
-				await item.save();
-				res.status(200).json(item);
-			}
-		} catch (err) {
-			res.status(401).json('Invalid User Id');
-		}
-	} else {
-		return res.status(401).json('Invalid Original Url');
-	}
-});
+//app.get('api/item/:url(*)', async (req, res) => {
+//	const urlCode = req.params.url;
+//	return res.json({ urlCode });
+//	const item = await UrlShorten.findOne({ urlCode: urlCode });
+//	if (item) {
+//		return res.redirect(item.originalUrl);
+//	} else {
+//		return res.redirect(errorUrl);
+//	}
+//});
+const port = 5000;
 
 app.listen(port, function() {
 	console.log('Node.js is listening on port ' + port);
